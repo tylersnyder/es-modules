9aed8d38acaac5545a88faff24aa0876cf9481dc
Tyler Snyder <tyler@Tylers-MacBook-Pro-2.local>
2019-01-05 00:02:10 -0500

    add css with variable examples

--- js/views/about.mjs
+++ js/views/about.mjs
@@ -7,6 +7,7 @@ export default function about() {
         <li>Fetch API wrapper</li>
         <li>DOM manipulation helpers</li>
         <li>Path-based router</li>
+        <li>CSS Variables</li>
       </ul>
       <a href="where-is-it.html">Check this error out</a>
     </div>
