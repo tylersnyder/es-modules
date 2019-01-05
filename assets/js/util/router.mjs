63640ee86dcc398d0efb5e16358b3296c1b940cc
Tyler Snyder <tyler@Tylers-MacBook-Pro-2.local>
2019-01-05 12:13:12 -0500

    routes

--- assets/js/util/router.mjs
+++ assets/js/util/router.mjs
@@ -1,8 +1,8 @@
 import $ from './dom.mjs'
 
-export default async function router(views) {
+export default async function router(routes) {
   const path = window.location.pathname
-  const render = views[path] || views['/error']
+  const render = routes[path] || routes['/error']
   const view = await render()
   $('#app').html(view)
 }
